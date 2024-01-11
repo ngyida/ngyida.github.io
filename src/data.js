7710b41a319fa9f6f8d6a639aa0de3f6ac113226
ngyida <e0726193@u.nus.edu>
2024-01-12 01:51:23 +0800

    update projects data

--- src/data.js
+++ src/data.js
@@ -1,10 +1,17 @@
 export const projects_data = [
-    {
+  {
+    title: "My Shop",
+    subtitle: "Stripe | React | Node",
+    description: "An E-commerce Shop",
+    image: "./testPic.avif",
+    link: "https://github.com/ngyida/MyShop",
+  },  
+  {
       title: "Sensitive Chat App",
       subtitle: "TensorFlow | IBM Cloud | React | Node.js",
       description: "A messaging web application with emotional recognition and sentiment analysis capability",
       image: "./testPic.avif",
-    //   link: "https://",
+      link: "https://github.com/ngyida/SensitiveChatApp.git",
     },
     {
       title: "Nakamoto Bitcoin",
@@ -18,7 +25,7 @@ export const projects_data = [
       subtitle: "Java | JUnit | Gradle | Github Actions",
       description: "An application for students to track refrigerator items and simplify the meal prepping process in hostel",
       image: "./testPic.avif",
-    //   link: "https://",
+      link: "https://github.com/AY2223S2-CS2113T-T09-3/tp",
     },
 ];
 
